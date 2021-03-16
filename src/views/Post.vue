f4133838861fb4f872860b05c9bdb393e307540b
Croquette <947006402@qq.com>
2021-03-16 20:11:27 +0800

    谢谢爹

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -38,13 +38,13 @@
               {{ post.text }}
             </div>
             <div class="tt-item-info info-bottom">
-              <a href="#" class="tt-icon-btn">
+              <a href="javascript:void(0);" class="tt-icon-btn" @click="handlePostLike">
                 <i class="tt-icon"
                   ><svg><use xlink:href="#icon-like"></use></svg
                 ></i>
                 <span class="tt-text">{{ post.likes }}</span>
               </a>
-              <a href="#" class="tt-icon-btn">
+              <a href="javascript:void(0);" class="tt-icon-btn" @click="handlePostDislike">
                 <i class="tt-icon"
                   ><svg><use xlink:href="#icon-dislike"></use></svg
                 ></i>
@@ -78,11 +78,11 @@
               <p>{{ item.text }}</p>
             </div>
             <div class="tt-item-info info-bottom">
-              <a href="#" class="tt-icon-btn">
+              <a href="javascript:void(0);" class="tt-icon-btn" @click="handleCommentLike(item.id)">
                 <i class="tt-icon"><svg><use xlink:href="#icon-like"></use></svg></i>
                 <span class="tt-text">{{ item.likes }}</span>
               </a>
-              <a href="#" class="tt-icon-btn">
+              <a href="javascript:void(0);" class="tt-icon-btn" @click="handleCommentDislike(item.id)">
                 <i class="tt-icon"><svg><use xlink:href="#icon-dislike"></use></svg></i>
                 <span class="tt-text">{{ item.dislikes }}</span>
               </a>
@@ -101,6 +101,7 @@
           submitText="评论"
           wrapClassName="form-default"
           v-model="commentContent"
+          @submit="handleEditorSubmit"
         />
       </div>
     </div>
@@ -121,23 +122,88 @@ export default {
       author: {},
       commentList: [],
       commentContent: '',
+      actionLock: false,
+    }
+  },
+  computed: {
+    postId() {
+      return this.$route.params.id;
     }
   },
   mounted() {
-    this.getData();
+    this.getPostData();
   },
   methods: {
-    getData() {
-      this.$axios.get(`/api/post/${this.$route.params.id}`).then(res => {
-        this.post = res.data;
+    getPostData() {
+      this.$axios.get(`/api/post/${this.postId}`).then(res => {
+        this.post = res.data.data.post;
         this.author = this.post.author || {};
         if (JSON.stringify(this.author) === '{}') return;
-        this.$axios.get(`/api/comments`).then(res => {
-          this.commentList = res.data;
-        });
+        console.log(this.author)
+        if (this.postId === this.post.postId) this.getCommentList();
       }).catch(e => {
         console.error(e);
       });
+    },
+    getCommentList() {
+      this.commentList = [];
+      this.$axios
+        .get(`/api/comments`, { params: { postId: this.post.postId } })
+        .then(res => {
+          this.commentList = res.data.data.commentList;
+          // window.scrollTo(0, 0);
+        }).catch(e => {
+          console.error(e);
+        });
+    },
+    handleEditorSubmit() {
+      if (this.commentContent === '') { alert('内容不能为空'); return; }
+      if (!this.$store.state.isAuthorized) { alert('请先登录'); return; }
+      this.$axios.post('/api/comment', {
+        userId: this.$store.state.userInfo.userId,
+        postId: this.post.postId,
+        content: this.commentContent,
+      }).then(res => {
+        if (res.data.success === true) {
+          this.clearEditor();
+          this.getCommentList();
+        }
+      }).catch(e => {
+        console.error(e);
+      });
+    },
+    handlePostLike() {
+      this.actionHandler('like', 'post', { id: this.post.postId });
+    },
+    handlePostDislike() {
+      this.actionHandler('dislike', 'post', { id: this.post.postId });
+    },
+    handleCommentLike(commentId) {
+      this.actionHandler('like', 'comment', { id: commentId });
+    },
+    handleCommentDislike(commentId) {
+      this.actionHandler('dislike', 'comment', { id: commentId });
+    },
+    actionHandler(action, target, payload) {
+      if (this.actionLock) return;
+      let _this = this;
+      const allowedAction = ['like', 'dislike'];
+      if (allowedAction.includes(action)) {
+        this.actionLock = true;
+        this.$axios.get(`/api/${target}${payload.id ? `/${payload.id}` : ''}`, {
+          params: { action }
+        }).then(res => {
+          if (res.data.data.success === true) console.log(res.data.data)
+          else alert('error')
+        }).catch(e => {
+          console.error(e);
+        }).finally(() => {
+          _this.actionLock = false;
+        })
+      }
+    },
+    clearEditor() {
+      this.commentContent = '';
     }
   }
 };
